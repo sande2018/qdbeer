2124007b90d8a2003a0a12b23adea2e8da52debd
Sande <36633585+sande2018@users.noreply.github.com>
2019-04-05 10:13:31 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,28 +1,30 @@
 package main
 
-import	(
+import (
 	"fmt"
-	"net/http"
 	"io/ioutil"
+	"net/http"
 	"net/url"
 	"strings"
 )
-var phone,captcha string
-func main(){
+
+var phone, captcha string
+
+func main() {
 	fmt.Println("【流量领取助手】By：吾爱破解-永彬后花园")
 	fmt.Printf("联通用户每次可领取30MB全国流量,每月可领取10次。提交领取申请后,流量会在24小时内充值到账，领取成功后可登录联通手机营业厅APP查询,流量自到帐后即时生效。有效期为三个月,每月第一天和最后一天不能领取流量,其他时间均可正常领取。\n\n")
 	runmain()
 }
-func runmain(){
+func runmain() {
 
-	for	{
-	fmt.Printf("请输入手机号码：")
+	for {
+		fmt.Printf("请输入手机号码：")
 		_, err := fmt.Scanf("%s\n", &phone)
 		if err != nil {
 			fmt.Println(err)
-		}else if len(phone)!=11{
+		} else if len(phone) != 11 {
 			fmt.Println("手机号码格式错误，请重新输入。")
-		}else{
+		} else {
 			getCode()
 			break
 		}
@@ -30,14 +32,14 @@ func runmain(){
 	}
 }
 
-func getCode(){
+func getCode() {
 	url_getCode := "https://m.10010.com/god/AirCheckMessage/sendCaptcha"
 	form := url.Values{}
 	form.Add("phoneVal", phone)
 	form.Add("type", "21")
 	resp, err := http.PostForm(url_getCode, form)
 	if err != nil {
-			fmt.Println(err)
+		fmt.Println(err)
 	} else {
 		getRes, gerErr := ioutil.ReadAll(resp.Body)
 		if gerErr != nil {
@@ -49,31 +51,30 @@ func getCode(){
 			if GetCode_state == true {
 				fmt.Println("验证码发送成功")
 				getCOde_scanf()
-			}else{
-				if string(getRes) == "{}"{
-				fmt.Println("验证码发送失败，60秒内不可重复发送验证码")
-				runmain()
-				}else{
-				fmt.Printf("号码:%s，一天最多只能兑换三次流量，请明天再来\n",phone)
-				runmain()
+			} else {
+				if string(getRes) == "{}" {
+					fmt.Println("验证码发送失败，60秒内不可重复发送验证码")
+					runmain()
+				} else {
+					fmt.Printf("号码:%s，一天最多只能兑换三次流量，请明天再来\n", phone)
+					runmain()
 				}
 			}
-
 
 		}
 
 	}
 }
-func getCOde_scanf(){
-	for	{
+func getCOde_scanf() {
+	for {
 		fmt.Printf("请输入验证码：")
 		_, err := fmt.Scanf("%s\n", &captcha)
 		if err != nil {
 			fmt.Println(err)
-		}else if len(captcha)!=4{
+		} else if len(captcha) != 4 {
 			fmt.Println("验证码格式错误")
 			getCOde_scanf()
-		}else{
+		} else {
 			flowExchange()
 			break
 		}
@@ -81,9 +82,7 @@ func getCOde_scanf(){
 	}
 }
 
-	
-
-func flowExchange(){
+func flowExchange() {
 	url_flowExchange := fmt.Sprintf("https://m.10010.com/god/qingPiCard/flowExchange?number=%s&type=%s&captcha=%s", phone, "21", captcha)
 	//fmt.Printf("%s\n", url)
 	get, err := http.Get(url_flowExchange)
@@ -98,10 +97,10 @@ func flowExchange(){
 		} else if strings.Contains(string(getRes), "1001") {
 			fmt.Println("验证码输入错误")
 			getCOde_scanf()
-		}else if strings.Contains(string(getRes), "0000") {
-			fmt.Printf("已成功向号码%s充值30M流量，3个月内有效。\n",phone)
+		} else if strings.Contains(string(getRes), "0000") {
+			fmt.Printf("已成功向号码%s充值30M流量，3个月内有效。\n", phone)
 			runmain()
-		}else {
+		} else {
 			fmt.Printf("未知错误，请稍后再试。\n", string(getRes))
 			runmain()
 		}
